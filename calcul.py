81a4a0d212740d78afc738eeca7680413ac2985b
Lilianc2000 <43184591+Lilianc2000@users.noreply.github.com>
2020-11-10 16:03:41 +0100

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -8,10 +8,10 @@
 
 def combat(a,b):
   if a == b:
-    return 0
+    return "0"
   elif (a == 1 and b == 2) or (a == 2 and b == 3) or (a == 3 and b == 1): #B win
     return "b"
   elif (a == 1 and b == 3) or (a == 2 and b == 1) or (a == 3 and b == 2): #A win
     return "a"
   else:
-    return -1
+    return "-1"
